71370313efca089df0b797b2f539293c7dab3b1a
Chepheus <anton.zamniborsch@milsdev.com>
2017-02-27 14:15:11 +0200

    Add capability to add frontend builder

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,10 @@ done
 if [ -f "$DATABASE_SCHEMA" ]
 then
     if [ -z "${DATABASE+xxx}" ]; then
+        echo "All Databases (mysql -h ${HOST_NAME} -u ${ROOT_USER} -p${ROOT_PASSWORD}  < ${DATABASE_SCHEMA})";
         mysql -h ${HOST_NAME} -u ${ROOT_USER} -p${ROOT_PASSWORD}  < ${DATABASE_SCHEMA};
     else
+        echo "Single Database (mysql -h ${HOST_NAME} -u ${ROOT_USER} -p${ROOT_PASSWORD} ${DATABASE} < ${DATABASE_SCHEMA})";
         mysql -h ${HOST_NAME} -u ${ROOT_USER} -p${ROOT_PASSWORD} ${DATABASE} < ${DATABASE_SCHEMA};
     fi
 else
@@ -27,11 +29,20 @@ touch style.css
 echo "NPM UPDATE";
 
 npm update
-./node_modules/.bin/grunt
+if [ -z ${FRONTEND_BUILDER+x} ]; then
+    echo "RUN node_modules/.bin/grunt";
+    ./node_modules/.bin/grunt
+else
+    echo "RUN ${FRONTEND_BUILDER}";
+    ${FRONTEND_BUILDER}
+fi
 
 cd ../../../
 rm -fv wp-content/mu-plugins/index.php
 
-./vendor/bin/sarcofag install
+if [ -f ./vendor/bin/sarcofag ]; then
+    ./vendor/bin/sarcofag install
+fi
 
+echo "Installation is complete!"
 while true; do sleep 1000; done
